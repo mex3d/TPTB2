8154e8fdea2328be528526199ae2042ce543445e
CHAI MAN XIAN 2100807C <2100807C@student.tp.edu.sg>
2023-02-05 23:47:54 +0800

    continue on wk12 u cant do it

---
+++ TPTB2/Client/Static/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TPTB2.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string UsersEndpoint = $"{Prefix}/users";
+        public static readonly string BookingsEndpoint = $"{Prefix}/bookings";
+        public static readonly string ReviewsEndpoint = $"{Prefix}/reviews";
+        public static readonly string PaymentsEndpoint = $"{Prefix}/payments";
+    }
+}
